24a6d9f59f458d4f69ae6bf30f231f7b80dcffe7
yssun <sales@yssun.com>
2013-12-13 10:58:21 +0800

    d

--- src/com/iteye/androidtoast/TwoActivity.java
+++ src/com/iteye/androidtoast/TwoActivity.java
@@ -9,7 +9,14 @@ public class TwoActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
+<<<<<<< Updated upstream
 		this.setContentView2(R.layout.twoactivity);//
+=======
+		
+		//
+		//
+		this.setContentView3333(R.layout.twoactivity);
+>>>>>>> Stashed changes
 		//TODO Auto-generated method stub
 		//aaaa
 	}
